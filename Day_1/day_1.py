f3aa21e43304399fa18b023536618acaefc6bfd9
Teo <boomir3856@gmail.com>
2024-12-23 16:05:08 +0100

    Task "Day 1" solution 	- implementation of code that finds total distance between 2 lists 	of locaiton IDs

--- Day_1/day_1.py
+++ Day_1/day_1.py
@@ -1,3 +1,36 @@
+import sys
+import os
+
+def help_page():
+    print("\nPROGRAM USAGE:\n"
+          "NAME\n"
+          "\tday_1.py - first task of AoC (find similarity and total distance)\n"
+          "SYNOPSIS\n"
+          "\tday_1.py [FILE PATH TO \".txt\" FILE]\n")
+
+def is_txt(path):
+    path_tuple = os.path.splitext(path)
+    return path_tuple[1].lower() == "txt" or path_tuple[1] == ""
+
+def cli():
+    if len(sys.argv) > 2 or len(sys.argv) < 2:
+        print("** Incorect number of arguments! **")
+        help_page()
+        exit()
+    else:
+        if is_txt(sys.argv[1]):
+            try:
+                open(sys.argv[1], 'r').close()
+            except FileNotFoundError:
+                print("** Specified file path is not correct! **")
+                exit()
+        else:
+            print("** Specified file is not a \"txt\" file! **")
+            help_page()
+            exit()
+
+    return sys.argv[1]
+
 def get_IDs(line):
     space_seen = 0
     num1, num2 = [], []
@@ -28,6 +61,22 @@ def get_input(path):
     
     return left_list, right_list
 
+def calc_similarity(left_list, right_list):
+    tot_similarity, cnt, i = 0, 0, 0
+
+    for num in left_list:
+        cnt, i = 0, 0
+        while i < len(right_list):
+            if right_list[i] == num:
+                cnt += 1
+                right_list.pop(i)
+            else:
+                i += 1
+        tot_similarity += cnt * num
+
+    return tot_similarity
+
+
 def find_minimum(list):
     minimum = list[0]
     idx = 0
@@ -54,14 +103,15 @@ def calc_distance(left_list, right_list):
 
         total_distance += abs(minl - minr)
     
-    return total_distance
+    return total_distance    
 
 def main():
-    path = "/home/teo/Documents/VS_py_projekti/AoC/Day_1/input"
+    path = cli()
+    left_list, right_list= get_input(path)
+    tot_distance = calc_distance(left_list.copy(), right_list.copy())
+    tot_similarity = calc_similarity(left_list.copy(), right_list.copy())
 
-    list1, list2 = get_input(path)
-    tot_distance = calc_distance(list1, list2)
+    print(f"Total distance is: {tot_distance}\n"
+          f"Total similarity is: {tot_similarity}")
 
-    print(f"Total distance is: {tot_distance}")
-
-main()
+main()  
