f8a2399b0a3d209280d8a23a14aba9086307c87d
Teo <boomir3856@gmail.com>
2024-12-27 17:58:11 +0100

    Day 2 of AoC: 	-Day 2 puzzle has been solved

--- Day_2/day_2.py
+++ Day_2/day_2.py
@@ -3,17 +3,18 @@ import os
 
 def help_page():
     print("\nPROGRAM USAGE:\n"
-          "NAME"
-          "\tday_2.py - Reindeeer nuclear powerplant puzzle solution"
-          "SYNOPSIS"
-          "\tday_2.py + [INPUT FILE PATH]")
+          "NAME\n"
+          "\tday_2.py - Reindeeer nuclear powerplant puzzle solution\n"
+          "SYNOPSIS\n"
+          "\tday_2.py + [INPUT FILE PATH] + [TOLERANCE]\n"
+          "\n\ttolerance - number of not safe values to be tolerated\n")
 
 def is_txt():
     path_tuple = os.path.splitext(sys.argv[1])
     return path_tuple[1].lower() == "txt" or path_tuple[1] == ""
 
 def cli():
-    if not 1 < len(sys.argv) < 3:
+    if not 1 < len(sys.argv) < 4:
         print("** Improper usage of program! **")
         help_page()
         exit()
@@ -21,8 +22,13 @@ def cli():
         if is_txt():
             try:
                 open(sys.argv[1], 'r').close()
+                tolerance = int(sys.argv[2])
             except FileExistsError:
                 print("** Specified path is not correct! **")
+                help_page()
+                exit()
+            except ValueError:
+                print("** Tolerance needs to be an integer! **")
                 help_page()
                 exit()
         else:
@@ -30,7 +36,7 @@ def cli():
             help_page()
             exit()
     
-    return sys.argv[1]
+    return sys.argv[1], tolerance
 
 def parse_str(str):
     num_list, num_str = [], []
@@ -52,41 +58,89 @@ def parse_str(str):
     
     return num_list
 
-def is_safe(num_list):
-    prev_diff = num_list[1] - num_list[0]
-    safe = 0
+def determine_bias(ok_signs):
+    # determines bias of the number list
+    # and makes a list of correct signs
+    if sum(ok_signs) > 0:
+        ascending = 1
+    else:
+        ascending = 0
+
+    return ascending
+
+def sign_condition(num_list):
+    signs = []
 
     for i in range(1, len(num_list)):
-        curr_diff = num_list[i] - num_list[i - 1]
+        diff = num_list[i] - num_list[i - 1]
 
-        if abs(curr_diff) < 4:
-            if curr_diff < 0 and prev_diff < 0:
-                safe = 1
-            elif curr_diff > 0 and prev_diff > 0:
-                safe = 1
-            else:
-                return 0
+        if diff > 0:
+            signs.append(1)
+        elif diff < 0:
+            signs.append(-1)
         else:
-            return 0
+            signs.append(0)
 
-        prev_diff = curr_diff
+    return determine_bias(signs)
 
-    return safe
+def is_safe(num_list, tolerance, tolerated, ascending):
+    end = len(num_list) - 1
+    start = 1
 
-def parse_input(path):
+    if tolerated > tolerance:
+        return False
+
+    for i in range(start, end):
+        diff = num_list[i] - num_list[i - 1]
+        nxt_diff = num_list[i + 1] - num_list[i]
+        num = num_list[i]
+
+        if ascending:
+            if 0 < diff < 4 and 0 < nxt_diff < 4:
+                continue
+            elif not 0 < diff < 4 and not 0 < nxt_diff < 4:
+                num_list.pop(i)
+            elif 0 < diff < 4 and not 0 < nxt_diff < 4:
+                if i < (end - 1) and num_list[i + 2] == num_list[i]:
+                       num_list.pop(i)
+                else:
+                    num_list.pop(i + 1)
+            else:
+                num_list.pop(i - 1)
+        else:
+            if -4 < diff < 0 and -4 < nxt_diff < 0:
+                continue
+            elif not -4 < diff < 0 and not -4 < nxt_diff < 0:
+                num_list.pop(i)
+            elif -4 < diff < 0 and not (-4 < nxt_diff < 0):
+                if i < (end - 1) and num_list[i + 2] == num_list[i]:
+                    num_list.pop(i)
+                else:
+                    num_list.pop(i + 1)
+            else:
+                num_list.pop(i - 1)
+
+        return is_safe(num_list, tolerance, tolerated + 1, ascending)
+    
+    return True
+
+def read_input(path, tolerance):
     file = open(path, 'r')
     safe_cnt = 0
 
     line = file.readline()
     while line:
-        if is_safe(parse_str(line)):
+        num_list = parse_str(line)
+        ascending = sign_condition(num_list)
+
+        if is_safe(num_list.copy(), tolerance, 0, ascending):
             safe_cnt += 1
         line = file.readline()
     
     return safe_cnt
 
 def main():
-    path = cli()
-    print("Num of safe reports:", parse_input(path))
+    path, tolerance = cli()
+    print("Num of safe reports:", read_input(path, tolerance))
 
-main()  
+main()
